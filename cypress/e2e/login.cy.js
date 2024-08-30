43fdba1830f12010c7eb238dd28620cd086f8be7
putrinada <putrinada1177@gmail.com>
2024-08-30 16:18:41 +0700

    Update README.md, signup.cy.js, and homePage.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,36 +1,35 @@
 const homePage = require('../support/pages/home-page/homePage')
 
-describe("Signup", () => {
+describe("Login", () => {
     beforeEach(() => {
         // pre step
         
         
     })
-
     it("with unregistered data", () => {
         homePage.goToHomePage()
         homePage.verifyHomePageAppears()
-        homePage.clickSignUpMenu()
-        homePage.verifySignUpModalAppears()
-        homePage.signUp('', 'putri123')
+        homePage.clickLoginMenu()
+        homePage.verifyLoginModalAppears()
+        homePage.Login('', 'putri123')
         homePage.verifyEmptyCredsErrorMessageAppears()
     })
 
     it("with empty data", () => {
         homePage.goToHomePage()
         homePage.verifyHomePageAppears()
-        homePage.clickSignUpMenu()
-        homePage.verifySignUpModalAppears()
-        homePage.signUp('', 'putri123')
+        homePage.clickLoginMenu()
+        homePage.verifyLoginModalAppears()
+        homePage.Login('', 'putri123')
         homePage.verifyEmptyCredsErrorMessageAppears()
     })
 
-    it("with registered data", () => {
+    it("with Valid data", () => {
         homePage.goToHomePage()
         homePage.verifyHomePageAppears()
-        homePage.clickSignUpMenu()
-        homePage.verifySignUpModalAppears()
-        homePage.signUp('random', 'putri123')
-        homePage.verifySignUpSuccessMessageAppears()
+        homePage.clickLoginMenu()
+        homePage.verifyLoginModalAppears()
+        homePage.Login('random', 'putri123')
+        homePage.verifyLoginSuccessMessageAppears()
     })
 })
--- cypress/support/pages/home-page/homePage.js
+++ cypress/support/pages/home-page/homePage.js
@@ -10,7 +10,7 @@ class homePage {
     verifyHomePageAppears() {
         cy.xpath(locators.datatestid.imageCompanyLogo).should('be.visible');
     }
-
+// fitur Sign up
     clickSignUpMenu() {
         cy.xpath('//*[@id="signin2"]').click();
     }
@@ -61,6 +61,60 @@ class homePage {
         }
         this.clickSignUpButton()
     }
+// Fitur Login 
+    clickLoginMenu() {
+        cy.xpath('//*[@id="login2"]').click();
+    }
+
+    verifyLoginModalAppears() {
+        cy.xpath('//*[@id="logInModalLabel"]', { timeout: 1000 }).should('be.visible');
+    }
+
+    fillUsername(username) {
+        if (username == 'random') {
+            username = faker.person.firstName() + faker.number.int() + '@test.com'
+        }
+        cy.wait(5000);
+        cy.xpath('//*[@id="loginusername"]', { timeout: 1000 }).should('be.visible');
+        cy.xpath('//*[@id="loginusername"]', { timeout: 5000}).type(username);
+    }
+
+    fillPassword(password) {
+        cy.xpath('//*[@id="loginpassword"]').type(password);
+    }
+
+    clickLoginButton() {
+        cy.xpath(locators.datatestid.button('Login')).click();
+    }
+
+    verifyAlertAppears(errorMessage) {
+        cy.on('window:alert',(t)=>{
+            expect(t).to.contains(errorMessage);
+         })
+    }
+
+    verifyEmptyCredsErrorMessageAppears() {
+        this.verifyAlertAppears(staticVars.error_message.empty_creds)
+    }
+
+    verifyUserAlreadyExistsErrorMessageAppears() {
+        this.verifyAlertAppears(staticVars.error_message.user_alr_exists)
+    }
+
+    verifyLoginSuccessMessageAppears() {
+        this.verifyAlertAppears(staticVars.success_message.login)
+    }
+
+    login(username, password) {
+        if (username != '') {
+            this.fillUsername(username);
+            this.fillPassword(password);
+        }
+        this.clickLoginButton()
+    }
+// Fitur Cart
+
+
 }
 
 module.exports = new homePage();
